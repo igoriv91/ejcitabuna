9b2f193202a335bfb6a903340994102038aae6da
IgorJanu <igor@intellisys.com.br>
2024-07-19 18:09:49 -0300

    tailwind

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-black">
+      <div></div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        papel: "url('/assets/bg1.png')",
+      },
+    },
+  },
+  plugins: [],
+};
